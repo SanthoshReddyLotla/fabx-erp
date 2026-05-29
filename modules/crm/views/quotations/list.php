6259f8a4628a2aff8734d82302ad2db79fdb88f6
santhosh <lsanthosh1999@gmail.com>
2026-05-29 15:35:56 +0530

    added list.php in crm views

---
+++ modules/crm/views/quotations/list.php
@@ -0,0 +1,6 @@
+<?php include A_TE_PATH . '/header.php'; ?>
+<div class="container-fluid xl:px-8... ">
+    <h2>Quotations List</h2>
+    <!-- Add your table loop here for rendering the quotes data -->
+</div>
+<?php include A_TE_PATH . '/footer.php'; ?>
