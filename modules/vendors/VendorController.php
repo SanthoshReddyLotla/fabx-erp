b58cf860bc61b44bf6420bc09f214799bc298671
santhosh <lsanthosh1999@gmail.com>
2026-05-29 15:46:40 +0530

    sidenavbar issue solved

--- modules/vendors/VendorController.php
+++ modules/vendors/VendorController.php
@@ -17,7 +17,7 @@ class VendorController extends Controller {
         $this->requireCan('read');
         $page = (int)($_GET['page'] ?? 1);
         $status = input('status');
-        $where = "WHERE is_deleted = 0"; $params = [];
+        $where = "WHERE 1=1"; $params = [];
         if ($status) { $where .= " AND status = ?"; $params[] = $status; }
         
         $vendors = $this->db->fetchAll(
